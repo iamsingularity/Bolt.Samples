50bc76a5d666785210ebb2c4edaa6dea8cd90675
justkao <tomka.martin@gmail.com>
2015-12-01 11:20:59 +0100

    - Added new Sample project

---
+++ Bolt.Console.Sample/src/Bolt.Console.Sample/DummyContract.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+namespace Bolt.Console.Sample
+{
+    public class DummyContract : IDummyContract
+    {
+        private readonly ILogger<DummyContract> _logger;
+
+        public DummyContract(ILogger<DummyContract> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task<string> ExecuteAsync(string dummyData)
+        {
+            _logger.LogDebug("Executing Bolt contract ... ");
+
+            return Task.FromResult(dummyData);
+        }
+    }
+}
---
+++ Bolt.Console.Sample/src/Bolt.Console.Sample/IDummyContract.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Bolt.Console.Sample
+{
+    public interface IDummyContract
+    {
+        Task<string> ExecuteAsync(string dummyData);
+    }
+}
